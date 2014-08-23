a5a6d2147fa8bb60729402f6340ea1b827bb814c
 <Apple@deMacBook-Pro.local>
2014-08-23 22:45:11 +0800

    Initial Commit

---
+++ FinancialPlatform/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FinancialPlatform
+//
+//  Created by  on 14-8-23.
+//  Copyright (c) 2014年 yuanxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FinancialPlatform/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FinancialPlatform
+//
+//  Created by  on 14-8-23.
+//  Copyright (c) 2014年 yuanxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
